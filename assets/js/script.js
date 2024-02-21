cb9bdf0f2162d3d8635322065347368ebcacdbcd
alessandrodamato <alessandro042004@gmail.com>
2024-02-21 15:27:17 +0100

    [fix] option selections

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,17 @@ const discountJunior = 20;
 const discountSenior = 40;
 
 let inputDistance = document.querySelector('.input-distance');
-let btnGenera = document.querySelector('.btn-genera');
-let selectAge = document.querySelector('.select-age');
+let btnGenerate = document.querySelector('.btn-generate');
+let selectAdult = document.querySelector('.adult');
+let selectJunior = document.querySelector('.junior');
+let selectSenior = document.querySelector('.senior');
 
-
-btnGenera.addEventListener('click', function(){
+btnGenerate.addEventListener('click', function(){
   km = parseInt(inputDistance.value);
   console.log(km);
-
-  if(selectAge.value = 'junior'){
+  if(selectJunior.selected){
     console.log('sono piccolo')
-  } else if (selectAge.value = 'senior'){
+  } else if (selectSenior.selected){
     console.log('sono vecchio')
   } else {
     console.log('sono adulto')
