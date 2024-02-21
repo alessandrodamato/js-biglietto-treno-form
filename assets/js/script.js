d6aef6edb01a32775e423104ee8a170d69683f5f
alessandrodamato <alessandro042004@gmail.com>
2024-02-21 16:15:00 +0100

    [add] comments and order

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
+// dati fissi
 const priceKm = 0.21;
 const discountJunior = 20;
 const discountSenior = 40;
 
+// variabili dichiarate
 let km;
 let price;
 
@@ -11,18 +13,27 @@ let selectAdult = document.querySelector('.adult');
 let selectJunior = document.querySelector('.junior');
 let selectSenior = document.querySelector('.senior');
 
+// funzione del bottone che calcola il prezzo e eventuali sconti
 btnGenerate.addEventListener('click', function(){
+
   km = parseInt(inputDistance.value);
   price = km * priceKm;
+  
   if(selectJunior.selected){
+
     finalPrice = price - price * discountJunior / 100;
     console.log('Junior:', 'Prezzo: €', price, 'Prezzo Junior: €', finalPrice.toFixed(2));
+    
   } else if (selectSenior.selected){
+
     finalPrice = price - price * discountSenior / 100;
     console.log('Senior:', 'Prezzo: €', price, 'Prezzo Senior: €', finalPrice.toFixed(2));
+
   } else {
+
     finalPrice = price
     console.log('Adulto:', 'Prezzo: €', finalPrice.toFixed(2));
+    
   }
 })
 
